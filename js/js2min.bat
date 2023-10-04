deea41b45f1c8edf4946be1fa38739f6a8c10fd8
lhy6305 <78890774+lhy6305@users.noreply.github.com>
2023-10-05 00:29:20 +0800

    ssh auto update

--- js/js2min.bat
+++ js/js2min.bat
@@ -3,6 +3,18 @@ if %1 == "" (
 exit
 )
 cmd /c uglifyjs %1 -c -e -o temp.js
-del /f /s /q %1
-cmd /c uglifyjs temp.js -c -e -o %1
+
+cmd /c uglifyjs temp.js -c -e -o %~n1.min%~x1
+del /f /s /q temp.js
+cmd /c uglifyjs %~n1.min%~x1 -c -e -o temp.js
+del /f /s /q %~n1.min%~x1
+cmd /c uglifyjs temp.js -c -e -o %~n1.min%~x1
+del /f /s /q temp.js
+cmd /c uglifyjs %~n1.min%~x1 -c -e -o temp.js
+del /f /s /q %~n1.min%~x1
+cmd /c uglifyjs temp.js -c -e -o %~n1.min%~x1
+del /f /s /q temp.js
+cmd /c uglifyjs %~n1.min%~x1 -c -e -o temp.js
+del /f /s /q %~n1.min%~x1
+cmd /c uglifyjs temp.js -c -e -o %~n1.min%~x1
 del /f /s /q temp.js
